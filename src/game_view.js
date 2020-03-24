4f58ce6bebd20995ed84903674700ea5a8be93fb
irynamcb <iryna.mcbride@gmail.com>
2020-03-23 21:01:00 -0700

    Refactor GameView

--- src/game_view.js
+++ src/game_view.js
@@ -23,20 +23,10 @@ GameView.prototype.right = function (ship) {
     ship.power([1, 0]);
 }
 
-GameView.prototype.up = function (ship) {
-    ship.power([0, -1]);
-}
-
-GameView.prototype.down = function (ship) {
-    ship.power([0, 1]);
-}
-
 GameView.prototype.bindKeyHandlers = function () {
     const ship = this.game.ship;
     key('a', function () { GameView.prototype.left(ship) });
     key('d', function () { GameView.prototype.right(ship) });
-    key('s', function () { GameView.prototype.down(ship) });
-    key('w', function () { GameView.prototype.up(ship) });
     key('space', function () { ship.fireBullet() });
 }
 
