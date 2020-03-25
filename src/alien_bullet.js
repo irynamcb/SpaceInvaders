72700239aaaff483f3734423c766b1b7825bb469
irynamcb <iryna.mcbride@gmail.com>
2020-03-24 21:15:05 -0700

    Add Alien Bullets

---
+++ src/alien_bullet.js
@@ -0,0 +1,30 @@
+const MovingObject = require("./moving_object.js");
+const Util = require("./util.js");
+
+Util.inherits(MovingObject, AlienBullet);
+
+AlienBullet.COLOR = "purple";
+AlienBullet.RADIUS = 2;
+
+function AlienBullet(options) {
+
+    let moOptions = {
+        pos: options.pos,
+        color: Bullet.COLOR,
+        radius: Bullet.RADIUS,
+        vel: Util.randomVec(Math.random() * 10),
+        game: options.game
+    }
+    MovingObject.call(this, moOptions);
+
+}
+
+AlienBullet.prototype.checkBoundaryConditions = function () {
+    if (this.isOutOfBounds(this.pos)) {
+        this.game.remove(this);
+    }
+}
+
+AlienBullet.prototype.isWrappable = false;
+
+module.exports = AlienBullet;
